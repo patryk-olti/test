036cd445010b6ab4ea2fad29dfd53d024cb725cb
Patryk Oltuch <oltix@wp.pl>
2020-04-02 10:57:58 +0200

    Updates

---
+++ precache-manifest.389a2e59827ae5a8274ed4303fc0265d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc4b10ddc4babbcfc25019fe6f56b96e",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "035557ae6b2e1b975369",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1c2bfbba8bcc1095cc17",
+    "url": "/test/static/js/2.f36a1e8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.f36a1e8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "035557ae6b2e1b975369",
+    "url": "/test/static/js/main.c08c377f.chunk.js"
+  },
+  {
+    "revision": "fed30565d512f36eba29",
+    "url": "/test/static/js/runtime-main.f7b66f57.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
